f6237fe16a05b5c14fe7ddf56f617c9020751593
Satya Krishna Gorti <satyakrishna.gorti@gmail.com>
2014-10-11 17:33:28 +0530

    Testing with checkboxes

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,17 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def results
+=begin
+    @value = params[:key]
+    puts @value
+=end
+
+  puts
+  puts
+  puts
+  puts "sldjf;asldkfjas;ldfjas;ldkfja;sldkfj;alskdfj;aslkdfj;laskdfj;alskdjfa;sjdf;as"
+  puts params[:rock]
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'pages/home'
+
+  get 'pages/results'
+
+  post 'pages/results'
+
   resources :review_counts
 
   resources :reviews
